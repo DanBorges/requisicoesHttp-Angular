17d4f895af6b32d3f57bec2968e85b7e118085ba
Daniel Borges <daniel.lemes.borges@hmail.com>
2019-08-29 21:07:03 -0300

    primeiro commit

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,38 @@
+import { CursosService } from './../cursos.service';
 import { Component, OnInit } from '@angular/core';
+import { Curso } from '../curso';
+import { Observable, empty, Subject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
+  // cursos: Curso[];
 
-  constructor() { }
+  cursos$: Observable<Curso>;
+  error$ = new Subject<boolean>();
+
+  constructor(private service:CursosService) { }
 
   ngOnInit() {
+    this.onRefresh();
   }
+  onRefresh() {
+    this.cursos$ = this.service.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
+  }
+
+
 
 }
